a71b5701edae0c2906c804a495973f11e70beb86
VerviumStrike <verviumstrike@protonmail.com>
2020-10-16 19:44:15 -0700

    SearchScrape

--- searchscrape.js
+++ searchscrape.js
@@ -1,13 +1,22 @@
 //Requirements Importer
 const Express = require("express")
+const Serp = require("serp")
 
 //Variables
 const Port = process.env.PORT || 3500
 const Web = Express()
 
+//Function
+async function QueryScraper(Query, res){
+    res.send(Query)
+}
+
 //Express
 Web.get("/", function(req, res){
-    res.send("Hello World")
+    res.send("Visit https://searchscrapev.herokuapp.com/api/searchscrape/ then type the thing you want to search in /.")
+})
+Web.get("/api/searchscrape/:query", function(req, res){
+    QueryScraper(req.params.query, res)
 })
 Web.listen(Port, ()=>{
 console.log(`==============================
